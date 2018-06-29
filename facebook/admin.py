c9613f84daacc4dd14ff1d96100754da18fb1c6c
Dg <dogeunchoi@gmail.com>
2018-06-29 19:03:04 +0900

    Week 4 - Exercise 1

--- facebook/admin.py
+++ facebook/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from facebook.models import Article
+from facebook.models import Page
 
 admin.site.register(Article)
+admin.site.register(Page)
--- facebook/models.py
+++ facebook/models.py
@@ -16,6 +16,7 @@ class Page(models.Model):
     master      = models.CharField(max_length=120)
     name        = models.CharField(max_length=120)
     text        = models.TextField()
+    category    = models.CharField(max_length=120, default='')
     created_at  = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
