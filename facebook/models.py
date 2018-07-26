defbdb4072af576cff54b4a0138959a191ef20f2
Dg <dogeunchoi@gmail.com>
2018-07-26 14:40:15 +0900

    Week 6 - Stage 1

--- facebook/models.py
+++ facebook/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# 뉴스피드 모델(게시판형)
 class Article(models.Model):
     author      = models.CharField(max_length=120)
     title       = models.CharField(max_length=120)
@@ -10,6 +10,17 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+# 코멘트 모델
+class Comment(models.Model):
+    article     = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    author      = models.CharField(max_length=120)
+    text        = models.TextField()
+    password    = models.CharField(max_length=120)
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
 
 # Week 4 - Challenge 1
 class Page(models.Model):
