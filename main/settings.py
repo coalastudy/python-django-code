ea24116230507d4f104d3e3fca0272148f938bf9
Dg <dogeunchoi@gmail.com>
2018-07-26 19:16:18 +0900

    Week 6 - Stage 3

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=$invbc8rl^(lm5aj%okhxy3ec!jddgoccag-h^=fif01ty&kz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
